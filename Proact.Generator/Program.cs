4ab34d59a15e32726341c23e54941132ca17cb92
Philip Hjorth Christiansen <pch@mjolner.dk>
2023-08-19 01:01:48 +0200

    Generator

---
+++ Proact.Generator/Program.cs
@@ -0,0 +1,13 @@
+﻿using Proact.Core.Tag;
+using Proact.Generator;
+using static Proact.Generator.CodeGenerator;
+
+var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Tags.cs");
+File.WriteAllText(path, $@"
+using {typeof(HtmlNode).Namespace};
+
+namespace {typeof(Proact.Core.Tags).Namespace};
+
+{AddTagsClassWithStaticMethods(TagsMeta.Tags)}
+{AddTagClasses(TagsMeta.Tags)}
+");
